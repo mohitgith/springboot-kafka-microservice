e0880cc05eb34fbc55467da0561342c9bc87290f
Mohit Shrivastava <42516296+mohitgith@users.noreply.github.com>
2023-06-11 01:26:17 +0530

    Add files via upload

---
+++ base-domains/src/test/java/com/learning/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
